0fdf0c0b5b96bfeb5d3c3286c86932b9d71c1b1f
Marcia Allen <mallen57@email.phoenix.edu>
2016-11-06 17:34:22 -0500

    VectorsDecrypt
    VectorsDecrypt Initial Commit

---
+++ VectorsDecrypt/VectorsDecrypt.cpp
@@ -0,0 +1,63 @@
+// VectorsDecrypting.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<char> normalV(26);
+	vector<char> cipherV(26);
+	string toDec = "";
+	string beenDec = "";
+
+	int i = 0;
+
+	normalV.at(0) = 'a'; cipherV.at(0) = '!'; ++i;
+	normalV.at(1) = 'b'; cipherV.at(1) = '^'; ++i;
+	normalV.at(2) = 'c'; cipherV.at(2) = '&'; ++i;
+	normalV.at(3) = 'd'; cipherV.at(3) = '*'; ++i;
+	normalV.at(4) = 'e'; cipherV.at(4) = '@'; ++i;
+	normalV.at(5) = 'f'; cipherV.at(5) = '('; ++i;
+	normalV.at(6) = 'g'; cipherV.at(6) = ')'; ++i;
+	normalV.at(7) = 'h'; cipherV.at(7) = '-'; ++i;
+	normalV.at(8) = 'i'; cipherV.at(8) = '#'; ++i;
+	normalV.at(9) = 'j'; cipherV.at(9) = '_'; ++i;
+	normalV.at(10) = 'k'; cipherV.at(10) = '='; ++i;
+	normalV.at(11) = 'l'; cipherV.at(11) = '+'; ++i;
+	normalV.at(12) = 'm'; cipherV.at(12) = '['; ++i;
+	normalV.at(13) = 'n'; cipherV.at(13) = '{'; ++i;
+	normalV.at(14) = 'o'; cipherV.at(14) = '$'; ++i;
+	normalV.at(15) = 'p'; cipherV.at(15) = ']'; ++i;
+	normalV.at(16) = 'q'; cipherV.at(16) = '}'; ++i;
+	normalV.at(17) = 'r'; cipherV.at(17) = ';'; ++i;
+	normalV.at(18) = 's'; cipherV.at(18) = ':'; ++i;
+	normalV.at(19) = 't'; cipherV.at(19) = ','; ++i;
+	normalV.at(20) = 'u'; cipherV.at(20) = '%'; ++i;
+	normalV.at(21) = 'v'; cipherV.at(21) = '<'; ++i;
+	normalV.at(22) = 'w'; cipherV.at(22) = '.'; ++i;
+	normalV.at(23) = 'x'; cipherV.at(23) = '>'; ++i;
+	normalV.at(24) = 'y'; cipherV.at(24) = '/'; ++i;
+	normalV.at(25) = 'z'; cipherV.at(25) = '?'; ++i;
+
+	// Get secret message
+	do {
+		cout << "Enter a secret message: ";
+		getline(cin, toDec);
+	} while (toDec.length() == 0);
+
+	beenDec = toDec;
+
+	// Decrypt secret message
+	for (int i = 0; i < cipherV.at(0); i++) {
+		if (toDec.at(0) == cipherV.at(0)) {
+			beenDec.at(0) = normalV.at(0);
+		}
+
+		cout << "Decrypted message: " << beenDec << endl;
+
+		return 0;
+	}
+}
